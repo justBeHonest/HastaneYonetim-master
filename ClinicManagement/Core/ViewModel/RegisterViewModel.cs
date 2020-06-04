5eb38030084c576f06fa547f86a479324dc16d20
Ahmet Ali Aşık <ahmethilmiask@gmail.com>
2020-06-05 00:06:48 +0300

    Initial commit

---
+++ ClinicManagement/Core/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClinicManagement.Core.ViewModel
+{
+    public class RegisterViewModel
+    {
+        [Display(Name ="İsim")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Parola")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Parolayı Doğrula")]
+        [Compare("Password", ErrorMessage = "Parolalar Uyumsuz.")]
+        public string ConfirmPassword { get; set; }
+
+        public bool? IsActive { get; set; }
+
+    }
+}
